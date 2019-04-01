1f3dd25929542ab9ed28ad6ada02edff1d78a30c
arcinself <41665388+arcinself@users.noreply.github.com>
2019-04-02 01:57:30 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,62 @@
+class LinkedList 
+{ 
+    Node head;
+    static int count=0;
+    class Node 
+    { 
+        int data; 
+        Node next; 
+        Node(int d) 
+        { 
+            data = d; 
+            next = null; 
+        } 
+    }
+    void printMiddle() 
+    { 
+        Node slow_ptr = head; 
+        Node fast_ptr = head; 
+        if (head != null) 
+        { 
+            while (fast_ptr != null && fast_ptr.next != null) 
+            { 
+                fast_ptr = fast_ptr.next.next; 
+                slow_ptr = slow_ptr.next; 
+            } 
+            if(count%2==0)
+                System.out.println("The middle element is [" + 
+                                slow_ptr.next.data + "] \n");
+            else
+            System.out.println("The middle element is [" + 
+                                slow_ptr.data + "] \n"); 
+        } 
+    } 
+    public void push(int new_data) 
+    { 
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node; 
+        count++;
+    }
+    public void printList() 
+    { 
+        Node tnode = head; 
+        while (tnode != null) 
+        { 
+            System.out.print(tnode.data+"->"); 
+            tnode = tnode.next; 
+        } 
+        System.out.println("NULL"); 
+    } 
+  
+    public static void main(String [] args) 
+    { 
+        LinkedList llist = new LinkedList(); 
+        for (int i=5; i>0; --i) 
+        { 
+            llist.push(i); 
+            llist.printList(); 
+            llist.printMiddle(); 
+        } 
+    } 
+}
